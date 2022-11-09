8c3f61acd2f2648c6d8c9ca7fbaec777e985bcb8
Adam Stegienko <adam.stegienko1@gmail.com>
2022-11-09 23:41:33 +0100

    Infrastructure repo created; 2 separate ws for dev and prod

---
+++ environment/dev/dev-backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate2adam"
+    container_name       = "tfstate"
+    key                  = "dev-terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
